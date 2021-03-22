69edc43330ed0b930e22b0dd48b3c3ddc9b7f4e6
FabienGreard <grd.fabien@gmail.com>
2021-03-22 03:08:58 +0100

    style: responsive clean

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,15 +12,15 @@ export default function Home() {
     <div className="flex flex-col h-screen">
       <Header />
       <main className="flex flex-col items-center justify-center w-screen h-full space-y-10">
-        <h1 className="text-6xl my-6">{formatMessage({ id: 'greetings' })}</h1>
-        <ReactSVG className="animate-spin-slow" size={128} />
-        <p className="text-sm">
+        <h1 className="text-4xl my-6 md:text-6xl">{formatMessage({ id: 'greetings' })}</h1>
+        <ReactSVG className="animate-spin-slow w-24 h-24 md:w-32 md:h-32" />
+        <p className="text-sm md:text-base">
           {formatMessage({ id: 'editingMessage' })}
-          <code className="bg-yellow-100 px-1">pages/index.js</code>
+          <code className="bg-yellow-100 p-1 mx-1">pages/index.js</code>
         </p>
       </main>
       <footer className="flex items-center justify-center h-10">
-        <span className="text-sm">{`v.${VERSION} - ${formatDate(Date.now())}`}</span>
+        <span className="text-xs md:text-sm">{`v.${VERSION} - ${formatDate(Date.now())}`}</span>
       </footer>
     </div>
   );
