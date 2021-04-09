f13196b1231e086b0bb7ca586f9ab46144e6da6b
FabienGreard <grd.fabien@gmail.com>
2021-04-09 13:08:26 +0200

    ci: bundle analyser  and workflow
    --eslint config fix

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,17 +1,18 @@
 import { ComponentType } from 'react';
 import Link from 'next/link';
 
-export const withLink = <T extends Object>(Component: ComponentType<T>) => ({
-  href,
-  locale,
-  ...props
-}: T & { href: string; locale?: string }) => (
-  <Link href={href} locale={locale} passHref>
-    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-    <a className="outline-none">
-      <Component {...((props as unknown) as T)} />
-    </a>
-  </Link>
-);
+export const withLink = <T extends Record<never, unknown>>(Component: ComponentType<T>) => {
+  const HOC = ({ href, locale, ...props }: T & { href: string; locale?: string }) => (
+    <Link href={href} locale={locale} passHref>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a className="outline-none">
+        <Component {...((props as unknown) as T)} />
+      </a>
+    </Link>
+  );
+
+  HOC.displayName = `${Component.name}WithLink`;
+  return HOC;
+};
 
 export default Link;
