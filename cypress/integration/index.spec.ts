5d026b22c8764bc037bad490f295d0b5de6eed0a
FabienGreard <grd.fabien@gmail.com>
2021-04-09 23:44:27 +0200

    fix: sw cache post

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -3,11 +3,31 @@ import '@testing-library/cypress';
 describe('Homepage', () => {
   it('Should greet user (en)', () => {
     cy.visit('/');
-    cy.findByText(/^Hello/i).contains('Hello World !');
+    cy.findByText(/^Hello/i).contains('Hello !');
   });
 
   it('Should greet user (fr)', () => {
     cy.visit('/fr');
-    cy.findByText(/^Bonjour/i).contains('Bonjour le monde !');
+    cy.findByText(/^Bonjour/i).contains('Bonjour !');
+  });
+
+  it('Should greet user (en - ssg)', () => {
+    cy.visit('/');
+    cy.findByText(/^Hello/i).contains('Hello John Smith !');
+  });
+
+  it('Should greet user (fr - ssg)', () => {
+    cy.visit('/fr');
+    cy.findByText(/^Bonjour/i).contains('Bonjour John Smith !');
+  });
+
+  it('Should greet user (en - ssr)', () => {
+    cy.visit('/');
+    cy.findByText(/^Hello/i).contains('Hello John Smith !');
+  });
+
+  it('Should greet user (fr - ssr)', () => {
+    cy.visit('/fr');
+    cy.findByText(/^Bonjour/i).contains('Bonjour John Smith !');
   });
 });
